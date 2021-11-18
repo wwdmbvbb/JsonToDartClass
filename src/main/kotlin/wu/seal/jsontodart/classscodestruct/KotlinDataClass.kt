49ff7dafa0263e074b218737eed0410711590665
Marcel Zickler <marcel.zickler@apirious.com>
2021-11-18 10:38:46 +0100

    new style

--- src/main/kotlin/wu/seal/jsontodart/classscodestruct/KotlinDataClass.kt
+++ src/main/kotlin/wu/seal/jsontodart/classscodestruct/KotlinDataClass.kt
@@ -32,6 +32,7 @@ data class KotlinDataClass(
             properties.forEach { p ->
                 val code = p.getCode()
                 val addIndentCode = code.split("\n").joinToString("\n") { indent + it }
+                append("final")
                 append(addIndentCode)
                 append(";")
                 if (p.comment.isNotBlank()) append(" // ").append(getCommentCode(p.comment))
@@ -51,10 +52,10 @@ data class KotlinDataClass(
             append(indent).append(indent).append("return ").append(name).append("(").append("\n")
 
             properties.forEach { p ->
-                append(indent). append(indent). append(indent)
+                append(indent).append(indent).append(indent)
                 append(p.name).append(": ")
 
-                val valueGetter = "json['${p.name}']"
+                val valueGetter = "json['${p.name}'] as ${p.type}"
 
                 when {
                     p.isListType() && p.getGenericType().isPrimitiveType() -> {
@@ -78,14 +79,12 @@ data class KotlinDataClass(
             append(indent).append("}").append("\n\n")
 
             //toJson
-            append(indent).append("Map<String, dynamic> toJson() {\n")
-            append(indent).append(indent).append("final Map<String, dynamic> data = new Map<String, dynamic>();\n")
+            append(indent).append("Map<String, dynamic> toJson() =>\n")
+            append(indent).append(indent).append("Map<String, dynamic>{\n")
 
             properties.filter { it.isPrimitiveType() }.forEach { p ->
                 append(indent).append(indent)
-                val valueSetter = "data['${p.name}']"
-                append(valueSetter).append(" = ").append("this.${p.name}")
-                append(";")
+                append("'${p.name}' : ${p.name}, ")
                 append("\n")
             }
 
@@ -96,25 +95,26 @@ data class KotlinDataClass(
                 when {
                     p.isListType() && p.getGenericType().isPrimitiveType() -> {
                         append("if (this.${p.name} != null) {\n")
-                        append(indent).append(indent).append(indent).append(valueSetter).append(" = ").append("this.${p.name};\n")
+                        append(indent).append(indent).append(indent).append(valueSetter).append(" = ")
+                            .append("this.${p.name};\n")
                         append(indent).append(indent).append("}\n")
                     }
                     p.isListType() && !p.getGenericType().isPrimitiveType() -> {
                         append("if (this.${p.name} != null) {\n")
-                        append(indent).append(indent).append(indent).append(valueSetter).append(" = ").append("this.${p.name}.map((v) => v.toJson()).toList();\n")
+                        append(indent).append(indent).append(indent).append(valueSetter).append(" = ")
+                            .append("this.${p.name}.map((v) => v.toJson()).toList();\n")
                         append(indent).append(indent).append("}\n")
                     }
                     else -> {
                         append("if (this.${p.name} != null) {\n")
-                        append(indent).append(indent).append(indent).append(valueSetter).append(" = ").append("this.${p.name}.toJson();\n")
+                        append(indent).append(indent).append(indent).append(valueSetter).append(" = ")
+                            .append("this.${p.name}.toJson();\n")
                         append(indent).append(indent).append("}\n")
                     }
                 }
             }
-            append(indent).append(indent).append("return data;\n")
-            append(indent).append("}").append("\n")
+            append(indent).append("};").append("\n")
 
-            append("}")
 //            if (nestedClasses.isNotEmpty()) {
 //                append(" {")
 //                append("\n")
